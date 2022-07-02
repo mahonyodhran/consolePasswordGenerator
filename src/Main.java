0098484c966738de63193bc4dd83e5c2b5a50e86
Odhran <mahonyodhran@gmail.com>
2022-07-02 13:12:14 +0100

    removed unused import

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
-
 public class Main {
     public static void main(String[] args) {
         int passwordLength = getPasswordLength();
