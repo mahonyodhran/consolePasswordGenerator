4e389952cff3cfaecef52c3ec8d4289a6fa39058
Odhran <mahonyodhran@gmail.com>
2022-07-02 21:38:05 +0100

    added flow for reading the file

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         helper.pinCheck();
         int passwordLength = helper.getPasswordLength();
         String password = helper.generatePassword(passwordLength);
-        // set up separate flow to ask if you want to save it
         boolean save = helper.savePassword();
 
         if (save) {
@@ -12,5 +11,12 @@ public class Main {
         } else {
             System.out.println("Your password is: " + password);
         }
+
+        boolean read = helper.wantReadFile();
+        if (read) {
+            System.out.println(helper.readFile());
+        }
+
+        System.out.println("Thanks for using the system - Goodbye . . .");
     }
 }
