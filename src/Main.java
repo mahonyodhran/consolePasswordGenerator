b14d0619a8ca4b050bbdccc41ea7bd8940ca53b1
Odhran <mahonyodhran@gmail.com>
2022-07-02 12:52:16 +0100

    added input validation for password length / passwordgen for loop

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String password = generatePassword();
-
+        int passwordLength = getPasswordLength();
+        String password = generatePassword(passwordLength);
         System.out.println(password);
     }
 
-    static String generatePassword() {
-        return "password";
+    static int getPasswordLength() {
+        Scanner input = new Scanner(System.in);
+        int length = 0;
+
+        do {
+            System.out.print("How long would you like your password: ");
+            while (!input.hasNextInt()) {
+                System.out.println("Please enter a value greater than 5");
+                input.next();
+            }
+            length = input.nextInt();
+        } while (length <= 6);
+
+        input.close();
+        return length;
+    }
+
+    static String generatePassword(int passwordLength) {
+        String password = "";
+        for (int i = 0; i < passwordLength; i++) {
+            password += i;
+        }
+        return password;
     }
 }
